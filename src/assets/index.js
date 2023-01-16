0e418c46384336c3839f87db800d688d7e03dcc3
Victor Alberto <victoralbertomhp@gmail.com>
2023-01-16 13:27:09 -0600

    Add assets

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export { default as Gallow } from './hangman_0.svg';
+export { default as Head } from './hangman_1.svg';
+export { default as LArm } from './hangman_2.svg';
+export { default as RArm } from './hangman_3.svg';
+export { default as LLeg } from './hangman_4.svg';
+export { default as RLeg } from './hangman_5.svg';
