6f5a0a6b770e040c9ce59eb97c01d20fcba8c29b
Victor Alberto <victoralbertomhp@gmail.com>
2023-01-16 13:30:02 -0600

    Add initial configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import useFetch from './hooks/useFetch';
+import { useState, useEffect } from 'react';
+import { Hangman, Word, Keyboard } from './components';
 import './App.css';
 
 function App() {
-	return <div>Hola</div>;
+	const { request, data } = useFetch();
+
+	useEffect(() => {
+		request();
+	}, []);
+
+	const [guessedLetters, setGuessedLetters] = useState([]);
+
+	return (
+		<div className='max-w-4xl flex flex-col gap-2 mx-auto items-center'>
+			<Hangman />
+			<Word />
+			<Keyboard />
+		</div>
+	);
 }
 
 export default App;
