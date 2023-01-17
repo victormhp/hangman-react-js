36b29b0ef09d3c5aad4ecd3333a18254c7f0ba4b
Victor Alberto <victoralbertomhp@gmail.com>
2023-01-16 19:37:36 -0600

    Structure the whole page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,61 @@
 import useFetch from './hooks/useFetch';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { Hangman, Word, Keyboard } from './components';
 import './App.css';
 
 function App() {
-	const { request, data } = useFetch();
+	const wordToGuess = useFetch();
+	const [guessedLetters, setGuessedLetters] = useState([]);
+	const incorrectLetters = guessedLetters.filter(
+		letter => !wordToGuess.includes(letter)
+	);
+
+	const isLoser = incorrectLetters.length >= 5;
+	const isWinner = wordToGuess
+		.split('')
+		.every(letter => guessedLetters.includes(letter));
+	console.log(isLoser);
+	console.log(incorrectLetters);
+	const addGuessedLetter = useCallback(
+		letter => {
+			if (guessedLetters.includes(letter) || isLoser || isLoser) return;
+			setGuessedLetters(currLetters => [...currLetters, letter]);
+		},
+		[guessedLetters, isLoser, isWinner]
+	);
 
 	useEffect(() => {
-		request();
-	}, []);
+		const handler = event => {
+			const key = event.key;
 
-	const [guessedLetters, setGuessedLetters] = useState([]);
+			if (!key.match(/^[a-z]$/)) return;
+
+			event.preventDefault();
+			addGuessedLetter(key);
+		};
+
+		document.addEventListener('keypress', handler);
+
+		return () => {
+			document.removeEventListener('keypress', handler);
+		};
+	}, [guessedLetters]);
 
 	return (
-		<div className='max-w-4xl flex flex-col gap-2 mx-auto items-center'>
-			<Hangman />
-			<Word />
-			<Keyboard />
-		</div>
+		<main className='max-w-5xl flex flex-col gap-12 mx-auto my-8 justify-center items-center'>
+			<Hangman attempts={incorrectLetters.length} />
+			<Word
+				answer={isLoser}
+				guessedLetters={guessedLetters}
+				wordToGuess={wordToGuess}
+			/>
+			<Keyboard
+				disabled={isLoser || isWinner}
+				activeLetters={guessedLetters.filter(l => wordToGuess.includes(l))}
+				inactiveLetters={incorrectLetters}
+				addGuessedLetter={addGuessedLetter}
+			/>
+		</main>
 	);
 }
 
