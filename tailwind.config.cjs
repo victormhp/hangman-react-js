e85cc40c0ae202d449651a81645964f20781309b
Victor Alberto <victoralbertomhp@gmail.com>
2023-01-16 22:14:44 -0600

    Add more custom font families and sizes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,8 +4,12 @@ module.exports = {
 	theme: {
 		extend: {
 			fontSize: {
-				keyboard: 'clamp(0.875rem, 0.7361rem + 0.6173vw, 1.5rem);',
+				title: 'clamp(1.5rem, 1.3333rem + 0.7407vw, 2.25rem)',
 				word: 'clamp(3rem, 2.3333rem + 2.9630vw, 6rem)',
+				keyboard: 'clamp(0.875rem, 0.7361rem + 0.6173vw, 1.5rem);',
+			},
+			fontFamily: {
+				press: 'Press',
 			},
 		},
 	},
