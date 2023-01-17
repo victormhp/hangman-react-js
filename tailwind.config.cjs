ae4b818dce1d9ff435852c0f67001660b04f252b
Victor Alberto <victoralbertomhp@gmail.com>
2023-01-16 19:35:16 -0600

    Add custom font sizes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontSize: {
+				keyboard: 'clamp(0.875rem, 0.7361rem + 0.6173vw, 1.5rem);',
+				word: 'clamp(3rem, 2.3333rem + 2.9630vw, 6rem)',
+			},
+		},
 	},
 	plugins: [],
 };
