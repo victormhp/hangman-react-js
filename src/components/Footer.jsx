6b5ed9f07e4276767c67cb062a6e32049313a4ba
Victor Alberto <victoralbertomhp@gmail.com>
2023-01-17 00:49:49 -0600

    Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+	return (
+		<footer className='text-footer text-center absolute bottom-0 w-full'>
+			Made with ♥ by&nbsp;
+			<a href='https://github.com/victormhp/'>Victor Alberto</a>
+		</footer>
+	);
+}
+
+export default Footer;
